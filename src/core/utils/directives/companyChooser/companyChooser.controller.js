92d792ab7431bd9f866d30deea4ce3a3e06e39a9
Stewan Pacheco <talk@stpa.co>
2015-07-08 02:03:12 -0300

    parametro com opção para 'todas empresas'

--- src/core/utils/directives/companyChooser/companyChooser.controller.js
+++ src/core/utils/directives/companyChooser/companyChooser.controller.js
@@ -1,6 +1,7 @@
 'use strict';
-angular.module('core.utils').controller('CompanyChooserCtrl', /*@ngInject*/ function($rootScope, $scope) {
-    var vm = this;
+angular.module('core.utils').controller('CompanyChooserCtrl', /*@ngInject*/ function($rootScope, $scope, lodash) {
+    var vm = this,
+        _ = lodash;
     vm.companyid = $scope.companyid;
     //external scope databind
     $scope.$watch('companyid', function(nv, ov) {
@@ -15,4 +16,25 @@ angular.module('core.utils').controller('CompanyChooserCtrl', /*@ngInject*/ func
             $rootScope.$emit('$CompanyIdUpdated', nv, ov);
         }
     });
+    //
+    // Add options for all companies
+    // https://github.com/esgrupo/livejob/issues/23
+    //
+    if ($user.instance().role.length > 1 && $scope.showAllOption) {
+        var allcompanies = [],
+            already = _.findIndex($scope.companies, function(row) {
+                return row.company.name === 'Todas Empresas';
+            });
+        if (already === -1) {
+            $user.instance().current('companies').forEach(function(row) {
+                allcompanies.push(row.company._id)
+            })
+            $scope.companies.push({
+                company: {
+                    name: 'Todas Empresas',
+                    _id: allcompanies
+                }
+            });
+        }
+    }
 });
--- src/core/utils/directives/companyChooser/companyChooser.directive.js
+++ src/core/utils/directives/companyChooser/companyChooser.directive.js
@@ -5,33 +5,13 @@ angular.module('core.utils').directive('companyChooser', /*@ngInject*/ function(
             companyid: '=',
             companies: '=',
             hideMe: '=',
-            placeholder: '='
+            placeholder: '=',
+            showAllOption: '=' //mostrar opção "todas empresas"
         },
         replace: true,
         restrict: 'EA',
         controller: 'CompanyChooserCtrl',
         controllerAs: 'vm',
         templateUrl: 'core/utils/directives/companyChooser/companyChooser.tpl.html'
-            // link: function($scope, $elem) {
-            //     //acompanhando issue no github https://github.com/angular/material/issues/2114
-            //     //quando o model é alterado, as vezes, ele adiciona "," repetindo o valor corrente. Ex: "Shopping Boulevard, Shopping Boulevard"
-            //     // $scope.$watch('companyid', function() {
-            //     //     var elem = $elem[0],
-            //     //         random = randomString(10),
-            //     //         timeout = [];
-            //     //     //  timeout[random] = setInterval(function() {
-            //     //     $scope.$apply(function() {
-            //     //         var company = $(elem).find('md-select-label span').first().text();
-            //     //         var split = company.split(',');
-            //     //         $(elem).find('md-select-label span').first().text(split[0]);
-            //     //            // $(elem).find('md-select').hide().show();
-            //     //          clearInterval(timeout[random]);
-            //     //     });
-            //     //     //  }, 3000);
-            //     // }, true);
-            //     function randomString(length) {
-            //         return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-            //     }
-            // }
     }
 });
