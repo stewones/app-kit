e45fa3720b456e4b6a6df4011e5ab6a92588b249
Matheus Davidson <matheusdavidson@gmail.com>
2015-07-16 15:05:07 -0300

    #6: adicionar template url na diretiva registerForm, done!

--- src/app-kit.js
+++ src/app-kit.js
@@ -2796,12 +2796,25 @@ angular.module('core.login').controller('RegisterFormCtrl', /*@ngInject*/ functi
 
 })
 'use strict';
+/**
+ * @ngdoc directive
+ * @name core.login.directive:registerForm
+ * @restrict E
+ * @description 
+ * Componente para o formulário de cadastro
+ * @element div
+ * @param {object} config objeto de configurações do módulo login
+ * @param {string} template-url caminho para o template do formulário
+ **/
 angular.module('core.login').directive('registerForm', /*@ngInject*/ function() {
     return {
         scope: {
-            config: '='
+            config: '=',
+            templateUrl: '='
         },
-        templateUrl: "core/login/register/registerForm.tpl.html",
+        templateUrl: function(elem, attr){
+            return attr.templateUrl ? attr.templateUrl : "core/login/register/registerForm.tpl.html";
+        },
         controller: 'RegisterFormCtrl',
         controlerAs: 'vm'
     }
