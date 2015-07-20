2b8e4aa34f5a9651f52a47b2b4bdd493606ad5c4
Stewan Pacheco <talk@stpa.co>
2015-07-20 13:32:21 -0300

    (fix/toolbarAvatar) melhor jogar pra rota de logout pra evitar problema com o md-menu

--- src/core/utils/directives/toolbarAvatar/toolbarAvatar.controller.js
+++ src/core/utils/directives/toolbarAvatar/toolbarAvatar.controller.js
@@ -1,9 +1,9 @@
 'use strict';
-angular.module('core.utils').controller('ToolbarAvatarCtrl', /*@ngInject*/ function($user) {
+angular.module('core.utils').controller('ToolbarAvatarCtrl', /*@ngInject*/ function($location) {
     var vm = this;
     vm.logout = logout;
 
     function logout() {
-        $user.instance().destroy();
+        $location.path('/logout/');
     }
 })
