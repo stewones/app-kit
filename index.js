13d3ab59b752dcab3bb93513044086a4e3984629
stewones <talk@stpa.co>
2015-08-29 17:15:42 -0300

    oops

---
+++ index.js
@@ -0,0 +1,4 @@
+var Comission = require('./src/core/utils/services/commission/commission');
+module.exports = {
+    Comission: Comission
+};
--- src/core/index.module.js
+++ src/core/index.module.js
@@ -1,26 +1,18 @@
 'use strict';
-var window = window ? window : {};
-if (window && window.length !== undefined) {
-    /**
-     * @ngdoc overview
-     * @name app.kit
-     * @description
-     * Kit para criação de aplicações frontend com angular 1.x <br />
-     * Serviços dos módulos com namespace "core" são identificados pelo prefixo $
-     **/
-    angular.module('app.kit', ['ngMaterial',
-    						   'ngAnimate',
-    						   'ngTouch',
-    						   'ngSanitize',
-    						   'angulartics',
-    						   'angulartics.google.analytics',
-    						   'ui.router',
-    						   'core.app']);
-}
-//
-// Node.js
-//
-else {
-    var Commission = require('utils/services/commission/commission');
-    module.exports = Commission;
-}
+/**
+ * @ngdoc overview
+ * @name app.kit
+ * @description
+ * Kit para criação de aplicações frontend com angular 1.x <br />
+ * Serviços dos módulos com namespace "core" são identificados pelo prefixo $
+ **/
+angular.module('app.kit', [
+    'ngMaterial',
+    'ngAnimate',
+    'ngTouch',
+    'ngSanitize',
+    'angulartics',
+    'angulartics.google.analytics',
+    'ui.router',
+    'core.app'
+]);
